fb11be9c600597eeb36f5c0567648502c7444d22
DipankarKundu1001 <dipankark31@gmail.com>
2018-03-18 22:53:48 +0530

    pattern

---
+++ Pattern2.java
@@ -0,0 +1,34 @@
+package org.patern.Stars;
+
+import java.util.Scanner;
+
+/*
+		* 
+		* ** 
+		* ** *** 
+		* ** *** **** 
+		* ** *** **** ***** 
+
+ */
+public class Pattern2 
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the numbers of lines :- ");
+		int n = sc.nextInt();
+		sc.close();
+		
+		for(int i = 0; i < n; i++)
+		{
+			for(int j = 0; j <= i ; j++)
+			{
+				for(int k = 0; k <= j; k++)
+					System.out.print("*");
+				System.out.print(" ");
+			}
+			System.out.println();
+		}
+	}
+
+}
